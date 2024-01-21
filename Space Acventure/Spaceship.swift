11e0903e70befbcd65ea778d643abc9b2a03ae76
imchino <chino618@me.com>
2024-01-21 16:39:02 +0900

    Step 5 Spaceshipクラスにactivate()メソッドを実装する main.swiftファイルのコードを抽象化する

--- Space Acventure/Spaceship.swift
+++ Space Acventure/Spaceship.swift
@@ -14,4 +14,35 @@ class Spaceship {
     init(name: String) {
         self.name = name
     }
+    
+    func activate() {
+        let numberOfPlanets = 8
+        let diameterOfEarth = 12756.274
+
+        print("Here is the solar system!")
+        print("There are \(numberOfPlanets) planets to explore.")
+        print("Diameter of The Earth is \(diameterOfEarth) km.")
+
+        print("What is your name?")
+        let yourName = readLine()!
+        print("Hi, \(yourName).")
+
+        print("Choose a planet to explore, randomly? (Y or N)")
+        var decision = ""
+
+        while !(decision == "Y" || decision == "N") {
+            decision = readLine()!
+            
+            switch decision {
+            case "Y":
+                print("Ok! Exploring planet randomly...")
+                // TODO: explore to random planet
+            case "N":
+                print("Ok, please select a planet to explore...")
+                // TODO: let the user select a planet to explore
+            default:
+                print("Sorry, I didn't get that.")
+            }
+        }
+    }
 }
--- Space Acventure/main.swift
+++ Space Acventure/main.swift
@@ -7,33 +7,5 @@
 
 import Foundation
 
-let numberOfPlanets = 8
-let diameterOfEarth = 12756.274
-
-print("Here is the solar system!")
-print("There are \(numberOfPlanets) planets to explore.")
-print("Diameter of The Earth is \(diameterOfEarth) km.")
-
-print("What is your name?")
-let yourName = readLine()!
-print("Hi, \(yourName).")
-
-print("Choose a planet to explore, randomly? (Y or N)")
-var decision = ""
-
-while !(decision == "Y" || decision == "N") {
-    decision = readLine()!
-    
-    switch decision {
-    case "Y":
-        print("Ok! Exploring planet randomly...")
-        // TODO: explore to random planet
-    case "N":
-        print("Ok, please select a planet to explore...")
-        // TODO: let the user select a planet to explore
-    default:
-        print("Sorry, I didn't get that.")
-    }
-}
-
 let mySpaceship = Spaceship(name: "Millennium Falcon")
+mySpaceship.activate()
