868fb1c1e5d38792be34baa4019a37a1fbe32e7c
imchino <chino618@me.com>
2024-01-26 11:08:11 +0900

    # Step 15. リファクタリング 惑星インスタンスを初期化する際の「反復コード」をリファクタリングします。 データ構造を使って、コード内の繰り返しパターンを削減します。

--- Space Acventure/main.swift
+++ Space Acventure/main.swift
@@ -7,25 +7,16 @@
 
 import Foundation
 
-// TODO: These codes are iterative.
-let mercury = Planet(name: "Mercury", diameter: 4880)
-let venus   = Planet(name: "Venus",   diameter: 12_104)
-let earth   = Planet(name: "Earth",   diameter: 12_756)
-let mars    = Planet(name: "Mars",    diameter: 6_788)
-let jupiter = Planet(name: "Jupiter", diameter: 142_984)
-let saturn  = Planet(name: "Saturn",  diameter: 120_536)
-let uranus  = Planet(name: "Uranus",  diameter: 51_118)
-let neptune = Planet(name: "Neptune", diameter: 49_532)
+let planetData = [ "Mercury" : 4_880,
+                   "Venus"   : 12_104,
+                   "Earth"   : 12_756,
+                   "Mars"    : 6_788,
+                   "Jupiter" : 142_984,
+                   "Saturn"  : 120_536,
+                   "Uranus"  : 51_118,
+                   "Neptune" : 49_532 ]
 
-var planets = [Planet]()
-planets.append(mercury)
-planets.append(venus)
-planets.append(earth)
-planets.append(mars)
-planets.append(jupiter)
-planets.append(saturn)
-planets.append(uranus)
-planets.append(neptune)
+let planets = planetData.map { Planet(name: $0, diameter: $1) }
 
 let planetarySystem = PlanetarySystem(name: "Solar system", planets: planets)
 let mySpaceship = Spaceship(name: "Millennium Falcon", availablePlanetarySystem: planetarySystem)
